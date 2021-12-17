aa57ea5b148b9ea4125c5fc9513081156f9ea557
Rujin Shrestha <63194470+TheNyachhon@users.noreply.github.com>
2021-12-17 23:27:48 +0530

    navbar animation

--- public/index.js
+++ public/index.js
@@ -29,14 +29,14 @@ const header = document.querySelector("#tocollapse");
 // DROPDOWN Menu Display
 const dropdownIcon = document.querySelector('#dropdown-icon');
 dropdown.addEventListener('click', () => {
-    if (header.style.display === "block") { //if nav is visible then hide
-        header.style.height = "0";
-        header.style.display = "none";
+    if (header.style.visibility === "visible") { //if nav is visible then hide
+        header.style.visibility = "hidden";
+        header.style.transform = "translateY(-100%)"
         dropdownIcon.classList.toggle('fa-times')
         dropdownIcon.classList.toggle('fa-list')
     } else { //if nav is not visible , then display
-        header.style.height = "100vh";
-        header.style.display = "block";
+        header.style.visibility = "visible";
+        header.style.transform = "translateY(0)";
         dropdownIcon.classList.toggle('fa-times')
         dropdownIcon.classList.toggle('fa-list')
     }
